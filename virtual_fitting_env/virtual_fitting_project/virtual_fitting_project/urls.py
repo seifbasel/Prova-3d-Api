5ef9807faf8171257bdd85ed1f081c6648949788
seif basel <seifbasel950@gmail.com>
2024-05-23 01:58:49 +0300

    done images again

--- virtual_fitting_env/virtual_fitting_project/virtual_fitting_project/urls.py
+++ virtual_fitting_env/virtual_fitting_project/virtual_fitting_project/urls.py
@@ -36,3 +36,6 @@ urlpatterns = [
     path('cartitem/<int:pk>', CartItemRetrieveUpdateDestroyAPIView.as_view(), name='cartitem-detail'),
     path('checkout', CheckoutAPIView.as_view(), name='checkout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

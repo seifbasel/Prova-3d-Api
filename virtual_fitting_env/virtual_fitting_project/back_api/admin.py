d5d2b727b26f48617165f090faf660de4856c4f4
seif basel <seifbasel950@gmail.com>
2024-06-21 18:28:19 +0300

    add to admin review

--- virtual_fitting_env/virtual_fitting_project/back_api/admin.py
+++ virtual_fitting_env/virtual_fitting_project/back_api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile, Category, Product,Favorite,Cart, CartItem,Order,Comment
+from .models import UserProfile, Category, Product,Favorite,Cart, CartItem,Order,Comment,review
 
 admin.site.register(UserProfile)
 admin.site.register(Category)
@@ -9,4 +9,5 @@ admin.site.register(Cart)
 admin.site.register(CartItem)
 admin.site.register(Order)          
 admin.site.register(Comment)          
+admin.site.register(review)          
 
